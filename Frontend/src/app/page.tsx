474c00441523dccb67886d16e15e037e3645f6fb
VIKAS_PATEL <166948664+VikasPatel2004@users.noreply.github.com>
2025-09-03 17:52:28 +0530

    initial commit

---
+++ Frontend/src/app/page.tsx
@@ -0,0 +1,8 @@
+
+export default function HomePage() {
+  return (
+    <div className="bg-blue-500 text-white p-4 text-center">
+      Tailwind is working!
+    </div>
+  );
+}
---
+++ Frontend/tailwind.config.js
@@ -0,0 +1,8 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ['./src/**/*.{js,ts,jsx,tsx}'], // or wherever your components live
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
